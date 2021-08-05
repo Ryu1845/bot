1025fe8f63a8b79a76aff9bbc14f221afc0db13a
MarkKoz <KozlovMark@gmail.com>
2021-08-04 20:23:20 -0700

    Time: update tests for format_with_duration
    Mock out the function calls rather than trying to make the dependencies
    transparent and ending up with an enormous amount of test cases.

--- tests/bot/utils/test_time.py
+++ tests/bot/utils/test_time.py
@@ -268,67 +268,28 @@ class TimeTests(unittest.TestCase):
         mock_discord_timestamp.assert_called_once_with(10000, time.TimestampFormats.RELATIVE)
         self.assertEqual(actual, mock_discord_timestamp.return_value)
 
-    def test_format_with_duration_none_expiry(self):
-        """format_with_duration should work for None expiry."""
-        test_cases = (
-            (None, None, None, None),
+    def test_format_with_duration_timestamp_none(self):
+        """format_with_duration should return None if the timestamp is falsy."""
+        timestamps = (None, '')
+        args = ((None, "minutes", 3), (datetime.now(), "hours", 4))
 
-            # To make sure that date_from and max_units are not touched
-            (None, 'Why hello there!', None, None),
-            (None, None, float('inf'), None),
-            (None, 'Why hello there!', float('inf'), None),
-        )
+        for timestamp, (other, precision, max_units) in zip(timestamps, args):
+            with self.subTest(timestamp=timestamp, other=other, precision=precision, max_units=max_units):
+                self.assertEqual(time.format_with_duration(timestamp, other, precision, max_units), None)
 
-        for timestamp, other, max_units, expected in test_cases:
-            with self.subTest(timestamp=timestamp, other=other, max_units=max_units, expected=expected):
-                self.assertEqual(time.format_with_duration(timestamp, other, max_units), expected)
+    @autospec(time, "discord_timestamp", "get_delta", "humanize_delta", pass_mocks=False)
+    def test_format_with_duration(self):
+        """format_with_duration should use discord_timestamp, get_delta, and humanize_delta."""
+        # Unfortunately, making these dependencies transparent to tests would involve significant
+        # redundancies in testing inputs that these functions ultimately handle.
+        time.discord_timestamp.return_value = "<t:discord_timestamp:f>"
+        time.get_delta.return_value = relativedelta(days=3)
+        time.humanize_delta.return_value = "humanised delta"
 
-    def test_format_with_duration_custom_units(self):
-        """format_with_duration should work for custom max_units."""
-        test_cases = (
-            ('3000-12-12T00:01:00Z', datetime(3000, 12, 11, 12, 5, 5), 6,
-             '<t:32533488060:f> (11 hours, 55 minutes and 55 seconds)'),
-            ('3000-11-23T20:09:00Z', datetime(3000, 4, 25, 20, 15), 20,
-             '<t:32531918940:f> (6 months, 28 days, 23 hours and 54 minutes)')
-        )
+        actual = time.format_with_duration("timestamp", "other", "precision", 9991238)
 
-        for timestamp, other, max_units, expected in test_cases:
-            with self.subTest(timestamp=timestamp, other=other, max_units=max_units, expected=expected):
-                self.assertEqual(time.format_with_duration(timestamp, other, max_units=max_units), expected)
-
-    def test_format_with_duration_normal_usage(self):
-        """format_with_duration should work for normal usage, across various durations."""
-        test_cases = (
-            ('2019-12-12T00:01:00Z', datetime(2019, 12, 11, 12, 0, 5), '<t:1576108860:f> (12 hours and 55 seconds)'),
-            ('2019-12-12T00:00:00Z', datetime(2019, 12, 11, 23, 59), '<t:1576108800:f> (1 minute)'),
-            ('2019-11-23T20:09:00Z', datetime(2019, 11, 15, 20, 15), '<t:1574539740:f> (7 days and 23 hours)'),
-            ('2019-11-23T20:09:00Z', datetime(2019, 4, 25, 20, 15), '<t:1574539740:f> (6 months and 28 days)'),
-            ('2019-11-23T20:58:00Z', datetime(2019, 11, 23, 20, 53), '<t:1574542680:f> (5 minutes)'),
-            ('2019-11-24T00:00:00Z', datetime(2019, 11, 23, 23, 59, 0), '<t:1574553600:f> (1 minute)'),
-            ('2019-11-23T23:59:00Z', datetime(2017, 7, 21, 23, 0), '<t:1574553540:f> (2 years and 4 months)'),
-            ('2019-11-23T23:59:00Z', datetime(2019, 11, 23, 23, 49, 5), '<t:1574553540:f> (9 minutes and 55 seconds)'),
-            (None, datetime(2019, 11, 23, 23, 49, 5), None),
-        )
-
-        for timestamp, other, expected in test_cases:
-            with self.subTest(timestamp=timestamp, other=other, expected=expected):
-                self.assertEqual(time.format_with_duration(timestamp, other), expected)
-
-    def test_format_with_duration_different_types(self):
-        """Both format_with_duration timestamp args should support ISO 8601 strings and aware/naïve datetimes."""
-        test_cases = (
-            (
-                datetime(2019, 11, 23, 22, 9, tzinfo=timezone(timedelta(hours=2))),
-                datetime(2019, 11, 15, 20, 15, tzinfo=timezone.utc),
-                '<t:1574539740:f> (7 days and 23 hours)'
-            ),
-            (datetime(2019, 11, 23, 20, 58), '2019-11-23T20:53:00Z', '<t:1574542680:f> (5 minutes)'),
-            ('2019-11-24T00:00:00+00:00', '2019-11-23T21:59:00-02:00', '<t:1574553600:f> (1 minute)'),
-        )
-
-        for timestamp, other, expected in test_cases:
-            with self.subTest(timestamp=timestamp, other=other, expected=expected):
-                self.assertEqual(time.format_with_duration(timestamp, other), expected)
+        self.assertEqual(actual, f"{time.discord_timestamp.return_value} ({time.humanize_delta.return_value})")
+        time.humanize_delta.assert_called_once_with(abs(time.get_delta.return_value), "precision", 9991238)
 
     def test_until_expiration_with_duration_none_expiry(self):
         """until_expiration should work for None expiry."""
